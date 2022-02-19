477c4e83c16212aa420f458fd6e7cf8be9b363a9
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-02-19 14:49:12 +0000

    Refactor checkout script

--- checkout.sh
+++ checkout.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-git remote add -f hugo-remote git@github.com:gohugoio/hugoBasicExample.git
-git remote add -f images-remote git@github.com:Smaug123/static-site-images.git
-git remote add -f meta-remote git@github.com:Smaug123/static-site-metadata.git
-git remote add -f pdfs-remote git@github.com:Smaug123/static-site-pdfs.git
+get_remote() {
+    url_name="$1"
+    url_remote="$2"
+    git remote get-url "$url_name" 2>/dev/null 1>/dev/null || git remote add -f "$url_name" "$url_remote"
+}
+
+get_remote hugo-remote git@github.com:gohugoio/hugoBasicExample.git
+get_remote images-remote git@github.com:Smaug123/static-site-images.git
+get_remote meta-remote git@github.com:Smaug123/static-site-metadata.git
+get_remote pdfs-remote git@github.com:Smaug123/static-site-pdfs.git
 
 git checkout -b working
 
